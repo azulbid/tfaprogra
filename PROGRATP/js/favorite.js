658fdaa461a0ef5672f34dc2789cbc16670d68f6
azulbid <azulbid@gmail.com>
2022-11-13 15:25:29 -0300

    fav js

---
+++ PROGRATP/js/favorite.js
@@ -0,0 +1,42 @@
+let buscador = document.querySelector('formulario')
+let camp = document.querySelector('.buscadortxt')
+
+buscador.addEventListener('submit', function(event) {
+    event.preventDefault();
+    if (camp.value == '') {
+        alert('Ingrese película o serie a buscar')
+    }
+    else {
+        this.submit()
+    }
+    }
+)
+
+let contenedor = document.querySelector('.pcontenedor')
+let titulo = document.querySelector('.titfav')
+let peliculas = [];
+
+if (localStorage.getItem('peliculas') && localStorage.getItem('peliculas') != null){
+    peliculas = JSON.parse(localStorage.getItem('peliculas'));
+}
+else if (peliculas.length == 0){
+    titulo.innerHTML = 'No hay favoritos seleccionados'
+}
+else {
+    for (let i = 0; i<peliculas.length; i++) {
+        let urlTrack = //link 
+        + peliculas[i];
+        fetch(urlTrack) 
+            .then (function(response){
+                return response.json();
+            })
+            .tgen (function(datos) {
+                console.log(datos)
+                contenedor.innerHTML += ''
+// <article class = "textodebusq">
+// img src 
+// h3
+// br
+// cierro article
+            })
+        }}
