ae1783a1adbb440520829962eba4487a10f41ed4
azulbid <azulbid@gmail.com>
2022-11-16 19:03:01 -0300

    arregle las fotos, no los titulos

--- PROGRATP/js/sresults.js
+++ PROGRATP/js/sresults.js
@@ -1,7 +1,7 @@
 let query = location.search; //location search es toda la url de la pag la guardas en la variable query 
 let stringToObject = new URLSearchParams(query); //metes esa variable en el metodo, te devuelve todas las urls de la pag
 let aBuscar = stringToObject.get('Search'); // agarras search 
-let url = `https://api.themoviedb.org/3/search/movie?api_key=5d8d9a4eaf9e1d9b0b7f27344d895a3e&language=en-US&query=${aBuscar}&page=1&include_adult=false`
+let url = `https://api.themoviedb.org/3/search/movie?api_key=385115c8e9bd0bc996d46c69d38601de&language=en-US&query=${aBuscar}&page=1&include_adult=false`
 
 fetch(url)
     .then(function (response) {
