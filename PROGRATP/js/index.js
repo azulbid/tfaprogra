278dcf9bdb56c011ba8b868695eff81d62693a91
azulbid <azulbid@gmail.com>
2022-11-16 18:10:09 -0300

    faltan las fotos y reacomodar los titulos

--- PROGRATP/js/index.js
+++ PROGRATP/js/index.js
@@ -18,9 +18,9 @@ window.addEventListener('load', function() {
     })
 
 
-let url = "https://api.themoviedb.org/3/movie/popular?api_key=385115c8e9bd0bc996d46c69d38601de&language=en-US&page=1"
+let url1 = "https://api.themoviedb.org/3/movie/popular?api_key=385115c8e9bd0bc996d46c69d38601de&language=en-US&page=1"
 
-fetch(url)
+fetch(url1)
     .then(function(response){
         return response.json()
 
@@ -34,10 +34,10 @@ fetch(url)
         contenidoT +=
         `<ul class="caja"> 
         <a href="./detail_movie.html?id=${info[i].id }"> 
-        </a> 
+        <img class="fotos" src= ${info[i].genre_ids.poster_path}"></img> </a> 
         <a href="./detail_movie.html"class="names">${info[i].title}</a>  
         <a href=".detail_movie.html"></a> 
-        "class="names">${info[i].title}</a> 
+       "class="names">${info[i].title}</a> 
         </li>`
     }
         tContainer.innerHTML += contenidoT
@@ -45,35 +45,6 @@ fetch(url)
     .catch(function(error){
         console.log(error);
     })  
-
-//let url2 = "https://api.themoviedb.org/3/tv/top_rated?api_key=3D385115c8e9bd0bc996d46c69d38601de&language=en-US&page=1"
-
-/*fetch(url2)
-    .then(function(response){
-    return response.json()
-    
-    })
-    .then(function(data){
-        let info = data.results
-        console.log(info);
-        let pContainer= document.querySelector('.pelisyseriestop');
-        let contenidoP= '';
-
-        for(let i=0; i<info.length; i++){
-            contenidoP += `<li class="caja">
-            <a href="./detail_movie.html?id=${info[i].id}" >
-            <img class="fotos" src="./detail_genres.html?id=${info[i].poster_path}"></img>
-            </a>
-            <a href="./detail_serie.html?id=${info[i].id}" class="names">${info[i].original_title}</a>
-            <a href="./detail_genres.html?id=${info[i].id}"class="names">by ${info[i].original_title}</a>
-            </li>`
-            } 
-            pContainer.innerHTML += contenidoP
-    })
-    .catch(function(error){
-            console.log(error);
-        })  
-    })   */
 
     let url3 = "https://api.themoviedb.org/3/movie/popular?api_key=385115c8e9bd0bc996d46c69d38601de&language=en-US&page=1"
 
@@ -106,9 +77,9 @@ fetch(url)
 
 
 
-let url = "https://api.themoviedb.org/3/movie/top_rated?api_key=385115c8e9bd0bc996d46c69d38601de&language=en-US&page=1"
+let url2 = "https://api.themoviedb.org/3/movie/top_rated?api_key=385115c8e9bd0bc996d46c69d38601de&language=en-US&page=1"
 
-fetch(url)
+fetch(url2)
     .then(function(response){
         return response.json()
 
@@ -122,10 +93,10 @@ fetch(url)
         contenidoT +=
         `<ul class="caja"> 
         <a href="./detail_movie.html?id=${info[i].id }"> 
-        </a> 
+        <img class="fotos" src= ${info[i]}"></img> </a> 
         <a href="./detail_movie.html"class="names">${info[i].title}</a>  
         <a href=".detail_movie.html"></a> 
-        "class="names">${info[i].title}</a> 
+       "class="names">${info[i].title}</a> 
         </li>`
     }
         tContainer.innerHTML += contenidoT
