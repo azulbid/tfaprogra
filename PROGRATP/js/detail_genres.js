fe6eae42d432358211fa32165da053c87876f91e
azulbid <azulbid@gmail.com>
2022-11-18 12:13:16 -0300

    detail genres ok

--- PROGRATP/js/detail_genres.js
+++ PROGRATP/js/detail_genres.js
@@ -21,9 +21,9 @@ let queryString = location.search
 let queryStringToObject = new URLSearchParams(queryString); 
 let id = queryStringToObject.get('id');
 
-//para peliculas
-let urlGeneros2 = "https://api.themoviedb.org/3/genre/movie/list?api_key=385115c8e9bd0bc996d46c69d38601de&language=en-US"
-    fetch(urlGeneros2)
+//PARA PELICULAS
+let urlGeneros = `https://api.themoviedb.org/3/discover/movie?api_key=385115c8e9bd0bc996d46c69d38601de&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${id}&with_watch_monetization_types=flatrate`
+    fetch(urlGeneros)
     .then(function(response){
         return response.json()
 
@@ -31,20 +31,70 @@ let urlGeneros2 = "https://api.themoviedb.org/3/genre/movie/list?api_key=385115c
 
     .then (function(data){
         console.log(data);
+        let generoApi = data.results
         let bContainer= document.querySelector('.contenedorgral');
-        let contenidoT= 
-            `<article>
-            <h2>${data.genres.name}</h2>
-            <a href="> 
-            </article>`;
-
-
-            generosPageContainer.innerHTML += contenidoGenerosPage;
+        let contenidoT= ''
+        for (let i =0 ; i<5; i++){
+            contenidoT +=
+            `<article class="myArticles">
+            <a href="detalle-genero.html?id=${generoApi[i].id}">
+            <div class="bloque-item-lista">
+                <h1 id="tituloGenero" class="titulopelis">${generoApi[i].original_title}</h1>
+                <img id="imagenPelicula" class="imagenes" src="https://image.tmdb.org/t/p/w500${generoApi[i].poster_path}" alt="foto1">
+                <p id="textoPelicula" class="texto"> ${generoApi[i].overview}</p>
+                <p class="estreno">${generoApi[i].release_date} </p>
+            </div>
+             </a>
+            </article>`
+            
+        }
+        console.log(contenidoT);
+        bContainer.innerHTML = contenidoT
+    
+    })
         
             })
     
 
-.catch(function(error){
-    console.log(error);
-})  })
+    .catch(function(error){
+        console.log(error);
+    })  
 
+//PARA SERIES
+
+let urlGeneros = `https://api.themoviedb.org/3/discover/movie?api_key=385115c8e9bd0bc996d46c69d38601de&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${id}&with_watch_monetization_types=flatrate`
+    fetch(urlGeneros)
+    .then(function(response){
+        return response.json()
+
+        })
+
+    .then (function(data){
+        console.log(data);
+        let generoApi = data.results
+        let bContainer= document.querySelector('.contenedorgral');
+        let contenidoT= ''
+        for (let i =0 ; i<5; i++){
+            contenidoT +=
+            `<article class="myArticles">
+            <a href="detalle-genero.html?id=${generoApi[i].id}">
+            <div class="bloque-item-lista">
+                <h1 id="tituloGenero" class="titulopelis">${generoApi[i].original_title}</h1>
+                <img id="imagenPelicula" class="imagenes" src="https://image.tmdb.org/t/p/w500${generoApi[i].poster_path}" alt="foto1">
+                <p id="textoPelicula" class="texto"> ${generoApi[i].overview}</p>
+                <p class="estreno">${generoApi[i].release_date} </p>
+            </div>
+             </a>
+            </article>`
+            
+        }
+        console.log(contenidoT);
+        bContainer.innerHTML = contenidoT
+    
+    })
+        
+
+
+    .catch(function(error){
+        console.log(error);
+    })  
