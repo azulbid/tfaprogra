52ad3a2f76cf55ebcae874ef05181563f563fd0b
ZANFRAN13 <fzanbisignani@udesa.edu.ar>
2022-11-21 01:10:12 -0300

    correcciones html y sresults.js

--- PROGRATP/js/sresults.js
+++ PROGRATP/js/sresults.js
@@ -1,3 +1,14 @@
+/*window.addEventListener('load', function() {
+    let formulario = document.querySelector('form');
+    let buscador = document.querySelector('[name="search"]'); 
+    let aviso = document.querySelector('.aviso')
+    formulario.addEventListener('submit', function(e){
+        e.preventDefault();
+        if( buscador.value === ""){
+            aviso.innerText = 'El buscador está vacío';
+        } else {
+            this.submit();
+        }*/
 
 
 // falta darle formato 
